72e8bf8c35651427fdd188967d359610a467857c
AKaushik2710 <159285365+AKaushik2710@users.noreply.github.com>
2025-01-19 12:20:26 +0530

    Adding Comments to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,37 @@ import {Div, DivOpr} from './Components/Divs.jsx'
 import {Buttons, Button} from './Components/Buttons.jsx'
 import Input from './Components/Input.jsx'
 
+// App Displayer 
 function App() {
+//Initializing a Ref
   const myRef = useRef("");
+
+// State to Determine whether click is after result 
   const [afterCalc, setAfterResult] = useState(false);
+
+// State to set value to input Element 
   const [input, setInput] = useState(myRef.current.value);
+
+// Input value setting function 
   function handleInput(value, setter){
     handleClick(value,setter);
     setInput(myRef.current.value);
   }
+
+// Setting state to determine after result click functionality 
   function handleState(val){
     setAfterResult(val);
   }
+
+// Setting Click Functionality 
   function handleClick(value, input=false){
-    if(afterCalc){
+    if(afterCalc){ // setting after result click functionality to false for after result click 
       setAfterResult(false);
     }
-    if(input || afterCalc){
+    if(input || afterCalc){// value starts from same if inputting through keyboard in between on-screen typing
       myRef.current.value = value;
     }
-    else{
+    else{ // continuing the value where it left 
     myRef.current.value!=0? myRef.current.value += value : myRef.current.value = value;
   }
   }
