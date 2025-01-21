482ca7761b76779d9b46439287a69cdf6ff96157
AKaushik2710 <amankaushik2710ak@gmail.com>
2025-01-21 16:17:15 +0000

    History Added

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,5 +1,5 @@
 import '../App.css'
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { Context } from './Context.jsx';
 
 // Render Numeric Buttons
@@ -29,28 +29,31 @@ stateSetter : state setting function to check whether input value is after resul
 stateVal : value is after result or not 
 handleInput: Not used
 */
-    const {children, oprt,egRef, stateVal, stateSetter, handleInput} = props;
+    const {children, oprt,egRef, setResult, stateSetter, onClick} = props;
     const handleClick = useContext(Context);
-    const initialResult = {
-        history:[],
-        characters:[]
-    };
-    const [result, setResult] = useState(initialResult);
+
     
+
     function handleOperations(e){
         if(oprt){ // children== operators 
             stateSetter(()=>false); // determining value is before result 
             handleClick(children); // click functionality 
         }
         else{
-            const current = egRef.current.value; // getting current input for non-input operations
+            let current = egRef.current ? egRef.current.value : ''; // getting current input for non-input operations
             switch (e.target.value){
                 case "=":
-                    try {
-                        egRef.current.value = current ? eval(current) : 0; // set current ref value to 0 if ref is empty 
-                        setResult({...result, history : [...result.history, egRef.current.value]}); // creating result history 
+                    try { 
+                        const result = current ? eval(current) : 0;
+                        if(result % 1 !== 0){
+                            egRef.current.value = result.toFixed(2); // set current ref value to 0 if ref is empty 
+                        }
+                        else{
+                            egRef.current.value = result; // set result to input field
+                        }
                         stateSetter(true); // setting after result state true
-                    } catch {
+                        setResult(egRef.current.value, true); // creating result history 
+                    } catch(error){
                         console.error("Invalid Error")
                     }
                 break;
@@ -59,7 +62,7 @@ handleInput: Not used
                     try{
                         egRef.current.value = current ? current.slice(0, current.length-1) : 0; // removing a single character from input 
                     }
-                    catch{
+                    catch(error){
                         console.error("Invalid Error")
                     }
                 break;
@@ -68,11 +71,20 @@ handleInput: Not used
                     try{
                         egRef.current.value = 0; // removing all characters from input 
                     }
-                    catch{
+                    catch(error){
                         console.error("Invalid Error")
                     }
                 break;
 
+                case "Back" :
+                    try{
+                        console.log("wdfdf")
+                        onClick(true); // setting back functionality
+                    }
+                    catch(error){
+                        console.error("Invalid Error")
+                    }
+                break;
             }
         }
     }
